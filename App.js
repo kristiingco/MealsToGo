22a676638539b1909e9207961087a326e02fdb1e
Kristi <kristiana.d.ingco@gmail.com>
2023-04-09 22:33:02 -0700

    Add basic styling for template

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView, Text, View, Platform } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Our React Native blank canvas!</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "green",
+            justifyContent: "center",
+          }}
+        >
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
   );
 }
 
