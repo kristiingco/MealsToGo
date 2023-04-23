113a1e9062ba584029a725f5ed23c73f4be37be5
Kristi <kristiana.d.ingco@gmail.com>
2023-04-22 22:01:26 -0700

    Add skeleton

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import { StatusBar as ExpoStatusBar, StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, Text, View, Platform } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View
-          style={{
-            padding: 16,
-            backgroundColor: "green",
-            justifyContent: "center",
-          }}
-        >
-          <Searchbar placeholder='Search' />
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style='auto' />
     </>
   );
 }
-
-const styles = StyleSheet.create({});
